115890e4fc35eee242c3de8e579197303042fa63
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-24 23:55:54 +0100

    Aggiunge click navbar

--- src/components/header/AppNav.vue
+++ src/components/header/AppNav.vue
@@ -2,7 +2,13 @@
 export default {
   data() {
     return {
-        navList: ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP']
+        navList: ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP'],
+        navIndex: 0,
+    }
+  },
+  methods: {
+    getIndex(index){
+        this.navIndex = index;
     }
   }
 }
@@ -11,12 +17,26 @@ export default {
 <template>
 <nav>
     <ul>
-        <li v-for="link in navList">{{link}}</li>
+        <li v-for="link, index in navList" @click="getIndex(index)" :class="{active: index === navIndex}">{{link}}</li>
     </ul>
 </nav>
 </template>
 
 <style lang="scss" scoped>
+    .active {
+        position: relative;
+        color: var(--primary-color-dark);
+
+        &::after {
+            position: absolute;
+            content: '';
+            height: .3125rem;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: var(--primary-color-dark);
+        }
+    }
     ul {
         display: flex;
         list-style: none;
@@ -24,6 +44,7 @@ export default {
         li {
             cursor: pointer;
             padding: 3.125rem 0;
+            transition: all 0.3s ease-out;
         
             &:not(:last-child) {
             margin-right: 1.875rem;
